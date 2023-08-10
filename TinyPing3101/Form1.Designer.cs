cc88e9f254ec9c7fb3545dbe43a89f6b045ecf03
sinheung kim <s2120@e-mirim.hs.kr>
2023-08-10 14:02:55 +0900

    GUI 디자인 작업

--- TinyPing3101/Form1.Designer.cs
+++ TinyPing3101/Form1.Designer.cs
@@ -28,13 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 17);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(328, 415);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Pink;
+            this.button1.Location = new System.Drawing.Point(402, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(221, 95);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "모두 보기";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Pink;
+            this.button2.Location = new System.Drawing.Point(402, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(221, 95);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "10000원 이상 가격 비싼 순서대로 보기";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Pink;
+            this.button3.Location = new System.Drawing.Point(402, 229);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(221, 95);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "이름 내림차순 보기";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Pink;
+            this.button4.Location = new System.Drawing.Point(402, 337);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(221, 95);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "별 3개 이상의 인기도 순서대로 보기";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(649, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "3101 김신흥";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
